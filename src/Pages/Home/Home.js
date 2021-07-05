2e90a7237bf32ca5f4050b43753b98498dc0fdcb
RajenMaglaque <71877344+RajenMaglaque@users.noreply.github.com>
2021-07-05 10:19:15 +0800

    Add files via upload

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,10 @@ const Home = ({handleLogged}) => {
         <div>
             <Header isLogged={handleLogged}/>
             <h1>Welcome!</h1>
-            <h3> Name: Lenie Anne L. Perez</h3>
+            <h3> Name: Maglaque, Rajen Hanwel T.</h3>
             <h3> Section: 32E1</h3>
             <h3> Course: Bachelor of Science in Information and Technology</h3>
-            <h3> Hobbies: Drawing, writing stories, watching anime, playing video games, listening to music</h3>
+            <h3> Hobbies: Watching anime, playing video games, listening to music, Watching youtube</h3>
         </div>
     )
 }
